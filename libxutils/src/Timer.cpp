8fcb9359eab8a520eaf0c81dfed603b2bc802efa
Alin Tomescu <alinush@mit.edu>
2019-06-24 11:40:34 -0400

    oops, no commas while printing AveragingTimer::stddev()

--- libxutils/src/Timer.cpp
+++ libxutils/src/Timer.cpp
@@ -13,12 +13,12 @@
 std::ostream& operator<<(std::ostream& out, const AveragingTimer& t) {
     if(t.numIterations() > 0)
         out << t.name << ": "
-            << Utils::withCommas(t.averageLapTime()) << " microsec per lap, "
-            << "-/+ " 
-            << Utils::withCommas(t.stddev()) << " stddev, "
-            << Utils::withCommas(t.min.count()) << " min, " 
-            << Utils::withCommas(t.max.count()) << " max, "
-            << "(" << t.numIterations() << " laps)";
+            << Utils::withCommas(t.averageLapTime()) << " microsec per lap"
+            << ", -/+ "
+            << t.stddev() << " stddev"
+            << ", " << Utils::withCommas(t.min.count()) << " min"
+            << ", " << Utils::withCommas(t.max.count()) << " max"
+            << " (" << t.numIterations() << " laps)";
     else
         out << t.name << ": did not run any laps yet.";
     return out;
